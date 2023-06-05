4a4b0950816dad264ad7da44d8c3dc2263578ef8
Softygrangy <129508225+Softygrangy@users.noreply.github.com>
2023-06-05 19:22:53 +0500

    two

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿Console.WriteLine("Введите натуральное число больше 1:");
-int number = int.Parse(Console.ReadLine());
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-void NumberCounter (int number)
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
 {
-    if (number < 0) Console.Write($"{number} не натуральное число");
-    if (number == 0) return;
-    Console.Write("{0,4}", number);
-    NumberCounter (number - 1);
+    Console.Write(SumMN(m - 1, n));
 }
 
-NumberCounter(number);
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
